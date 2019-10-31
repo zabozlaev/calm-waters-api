9193bd9d5b11087ebb2391c955cb7703cf76382e
Ilya Zabcode <ilya@belkins.io>
2019-10-31 17:25:55 +0300

    [add] BASE URL IN ENV

--- src/auth/strategy/linkedin.strategy.ts
+++ src/auth/strategy/linkedin.strategy.ts
@@ -3,15 +3,13 @@ import { Strategy } from 'passport-linkedin-oauth2';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
-const BASE_URL = `https://calm-waters-99106.herokuapp.com`;
-
 @Injectable()
 export class LinkedInStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       clientID: process.env.LINKEDIN_CLIENT_ID,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      callbackURL: BASE_URL + '/auth/linkedin/callback',
+      callbackURL: process.env.BASE_URL + '/auth/linkedin/callback',
       passReqToCallback: true,
     });
   }
