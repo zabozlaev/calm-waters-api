e182461c028b3588737554ae9f3d516b66da79c8
Ilya <44939563+zabozlaev@users.noreply.github.com>
2019-12-09 14:58:05 +0300

    fixed a bug

--- src/user/user.decorator.ts
+++ src/user/user.decorator.ts
@@ -2,5 +2,5 @@ import { TokenPayload } from '../auth/dtos/tokenPayload';
 import { createParamDecorator } from '@nestjs/common';
 
 export const UserId = createParamDecorator((_, req) => {
-  return (req.user && req.user.id) || '123';
+  return (req.user && req.user.id);
 });
